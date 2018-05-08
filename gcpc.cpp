0c510bb6b2bc7fb28032d8c8e71abc60d5f5ee34
leonardochara <150402@unsaac.edu.pe>
2018-05-08 23:22:25 +0300

    v1

---
+++ gcpc.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <cstdio>
+
+
+using namespace std;
+
+int main(){
+
+	int n,m,team,penalty;
+	int arr[n];
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> team >> penalty;
+		    
+	}
+}
